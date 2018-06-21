3e28f8e92c2a9d5750308f8c6ba82cb23d510616
Crystal Buckneberg <crb@kcddagger.com>
2018-06-21 00:14:33 -0500

    challeng3 final code.

--- challenge3/Program.cs
+++ challenge3/Program.cs
@@ -10,8 +10,43 @@ namespace challenge3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            string n;
+            string strLN;
+            int lN;
+
+            Console.WriteLine("Enter a number 1-9");
+            n = Console.ReadLine();
+            strLN = largestNumber(n);
+
+            if (int.TryParse(strLN, out lN))
+            {
+                Console.WriteLine(lN);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine(strLN);
+                Console.ReadLine();
+            }
+        }
+
+        private static string largestNumber(string n)
+        {
+            int num;
+            string strLNum = "";
+
+            if ((int.TryParse(n, out num)) && (num > 0 && num <= 9))
+            {
+                for (int i = 0; i < num; i++)
+                {
+                    strLNum += "9";
+                }
+                return strLNum;
+            }
+            else
+            {
+                return "Invalid input.  Please restart the program and enter in a number 1-9";
+            }
         }
     }
 }
